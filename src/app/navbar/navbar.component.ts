8b2e3a072e04e463f538fb1fabcd4300157a0aa2
Bishop <christianiradukunda6@gmail.com>
2023-08-20 21:20:48 +0200

    storing the language from local storage to the lang variable

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,11 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css']
 })
-export class NavbarComponent {
+export class NavbarComponent implements OnInit {
+  lang;
+
+ ngOnInit(){
+  this.lang = localStorage.getItem('lang') || 'kin';
+  
+ }
 
 
   // adding the function to set the local storage language
